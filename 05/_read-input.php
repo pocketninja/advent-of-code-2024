91431f90c996f2bba8525788153292776273240e
Dan Herbert <daniel.eherbert@gmail.com>
2024-12-05 21:52:59 +1030

    Day 5 part 01

---
+++ 05/_read-input.php
@@ -0,0 +1,31 @@
+<?php
+
+$handle = fopen('./input/pages-and-rules.txt', 'r');
+
+$rules = [];
+$sequences = [];
+
+while (($line = fgets($handle)) !== false) {
+    $line = trim($line);
+
+    if (empty($line)) {
+        continue;
+    }
+
+    if (str_contains($line, '|')) {
+        $order = explode('|', $line);
+
+        if (!array_key_exists($order[0], $rules)) {
+            $rules[$order[0]] = [];
+        }
+
+        $rules[$order[0]][] = $order[1];
+    } else {
+        if (str_contains($line, ',')) {
+            $sequences[] = explode(',', $line);
+        }
+    }
+}
+
+return compact('rules', 'sequences');
+
--- 05/part-01.php
+++ 05/part-01.php
@@ -3,10 +3,53 @@ declare(strict_types=1);
 
 chdir(__DIR__);
 
-$grid = file_get_contents('./input/pages-and-rules.txt');
-
+['rules' => $rules, 'sequences' => $sequences] = require('_read-input.php');
 
 $answer = 0;
+
+foreach ($sequences as $sequence) {
+    $pass = true;
+
+    for ($i = 0; $i < count($sequence); $i++) {
+        $pageNumber = $sequence[$i];
+
+        if (!array_key_exists($pageNumber, $rules)) {
+            throw new Exception(sprintf('Invalid page number, no rule: %s', $pageNumber));
+        }
+
+        $pageRule = $rules[$pageNumber];
+
+        for ($x = $i; $x >= 0; $x--) {
+            if (in_array($sequence[$x], $pageRule)) {
+                printf("\n!! invalid sequence: %s; %d came before %d",
+                    implode(',', $sequence),
+                    $sequence[$x],
+                    $sequence[$i],
+                );
+                $pass = false;
+                break;
+            }
+        }
+
+        if (!$pass) {
+            break;
+        }
+
+    }
+
+    if (!$pass) {
+        continue;
+    }
+
+    $midIndex = floor(count($sequence) / 2);
+
+    printf("\n-> valid sequence: %s; [%d] => %d",
+        implode(',', $sequence),
+        $midIndex, $sequence[$midIndex],
+    );
+
+    $answer += $sequence[$midIndex];
+}
 
 file_put_contents('answer-part-01.txt', $answer);
 
